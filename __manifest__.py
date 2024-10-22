1a47a76b2c3181ddb5bce6fe35000049fa159273
Benji Codes <89851307+Thaura644@users.noreply.github.com>
2024-10-22 16:57:31 +0300

    done-ish

--- __manifest__.py
+++ __manifest__.py
@@ -16,18 +16,21 @@
         'security/ir.model.access.csv',
 
         'views/data.xml',
-        'views/ksba_bus_views.xml',
+        
+        
         'views/ksba_school_views.xml',
         'views/create_user_form.xml',
         'views/ksba_route_views.xml',
         'views/ksba_stop_views.xml',
+        'views/menu.xml',
+        'views/ksba_bus_views.xml',
         'views/ksba_child_views.xml',
         'views/ksba_driver_views.xml',
         'views/ksba_administrator_views.xml',
         'views/ksba_partner_views.xml',
         'views/ksba_attendance_record_views.xml',
         'views/ksba_bus_location_views.xml',
-        'views/menu.xml',
+        
         # 'views/ksba_menu_views.xml'
     ],
     'demo': [],
